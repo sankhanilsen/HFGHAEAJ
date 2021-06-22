d42dd0bcae432e75b966265cbf3c74b0c8ab4200
sankhanilsen <76916705+sankhanilsen@users.noreply.github.com>
2021-06-22 14:23:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 img="";
-
+status ="";
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 function preload(){
 img = loadImage('dog_cat.jpg');
@@ -14,5 +16,21 @@ function draw(){
     noFill();
     stroke("#fa007d");
     rect(520,50,100,130);
+
+  
+
 }
+function modelLoaded(){
+    console.log("MODEL LOADED ! ");
+     status = true;
+     objectDetector.detect(img , gotResult);
+
+}
+function gotResult(error,results){
+    if(error){
+     console.log(error);
+    }
+    console.log(results);
+}
+
 
